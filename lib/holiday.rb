dc4e81aa5be1685c2bb82222105558cc8ef760a9
Ji Yim <gsooyim@gmail.com>
2018-04-19 19:02:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
-      puts "#{season.upcase}\n #{holiday.upcase}: #{supplies.upcase}\n"
+      puts "#{season.upcase}\n #{holiday.upcase}: #{supplies.join(, )}\n"
     end
   end
 end
