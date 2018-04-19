b62166f021923ecd6e6d37c769810b079cc78158
Ji Yim <gsooyim@gmail.com>
2018-04-19 19:07:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,6 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
+      puts "#{holiday.capitalize}: #{supplies}"
     end
   end
 end
