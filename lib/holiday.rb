d28bfce4b78e0d686c71c33535e2327d9a004eac
Ji Yim <gsooyim@gmail.com>
2018-04-19 19:05:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,8 +56,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
+    puts "#{season}"
     holidays.each do |holiday, supplies|
-      puts "#{season}\n #{holiday}: #{supplies}\n"
     end
   end
 end
